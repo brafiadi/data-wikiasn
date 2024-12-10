99f219c6156e5db0f0d82f4c21cc3c9ca586f0fe
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2024-12-10 14:16:03 +0700

    add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "data-wikiasn", // Name of your application
+  script: "src/index.ts", // Entry point of your application
+  watch: true,
+  interpreter: "bun", // Path to the Bun interpreter
+};

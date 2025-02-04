6a6ceee2be0c50b5854a84d67ff6e0894c49e5d2
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2025-02-04 13:27:11 +0700

    update peraturan api

--- src/routes/peraturan.route.ts
+++ src/routes/peraturan.route.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
 import { PeraturanController } from "../controllers/peraturan.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 export const peraturanRoute = new Hono();
 const peraturanController = new PeraturanController();
 
 peraturanRoute.get("/", (c) => peraturanController.listPeraturan(c));
 peraturanRoute.get("/data", (c) => peraturanController.peraturanById(c));
+peraturanRoute.post("/", authMiddleware, (c) =>
+	peraturanController.insertPeraturan(c),
+);
